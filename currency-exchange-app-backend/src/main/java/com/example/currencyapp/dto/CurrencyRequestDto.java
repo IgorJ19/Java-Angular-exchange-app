220d5c830a6cd9bd9d1007c47ebb50cfe6b2f782
Igor <jurkiewiczigor19@gmail.com>
2024-07-18 20:08:42 +0200

    Delete currency-exchange-app-backend directory to put correct one

--- currency-exchange-app-backend/src/main/java/com/example/currencyapp/dto/CurrencyRequestDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.currencyapp.dto;
-
-//FOR FUTURE DEVELOPMENT, NOT USED BECAUSE THERE IS NO DATA TO PROTECT
-public class CurrencyRequestDto { }
--- currency-exchange-app-backend/src/main/java/com/example/currencyapp/repository/CurrencyRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.currencyapp.repository;
-
-import com.example.currencyapp.model.CurrencyRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CurrencyRequestRepository extends JpaRepository<CurrencyRequest, Long> { }
